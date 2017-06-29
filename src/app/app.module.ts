baa2874b6944d926a30c169ceb2823fbbd8a0698
kobe73er <262103206@qq.com>
2017-06-29 22:08:21 +0800

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,44 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {ContentComponent} from './content/content.component';
+
+import {RouterModule, Routes} from '@angular/router';
+import { Content2Component } from './content2/content2.component';
+import { NaviComponent } from './navi/navi.component';
+import { HomeComponent } from './home/home.component';
+import { PhotoComponent } from './photo/photo.component';
+
+const appRoutes: Routes = [
+  {path: 'content', component: ContentComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'photos', component: PhotoComponent},
+  // {path: 'hero/:id', component: Detail},
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {path: '**', component: HomeComponent}
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContentComponent,
+    Content2Component,
+    NaviComponent,
+    HomeComponent,
+    PhotoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/content/content.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.css']
+})
+export class ContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
